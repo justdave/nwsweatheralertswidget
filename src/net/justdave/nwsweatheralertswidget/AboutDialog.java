0098e232342bed22d04af059a3f7f389ac4bc6fb
David Miller <github@justdave.net>
2014-03-01 22:09:42 -0500

    Update version number to 1.0.2 Also updated the about box to use the version number from the manifest so I only have to update it in one place.

--- src/net/justdave/nwsweatheralertswidget/AboutDialog.java
+++ src/net/justdave/nwsweatheralertswidget/AboutDialog.java
@@ -7,9 +7,12 @@ import java.io.InputStreamReader;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.util.Linkify;
+import android.util.Log;
 import android.graphics.Color;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +20,8 @@ import android.widget.TextView;
 
 public class AboutDialog extends Dialog {
     private static Context mContext = null;
+    private static final String TAG = AboutDialog.class.getSimpleName();
+
     public AboutDialog(Context context) {
         super(context);
         mContext = context;
@@ -26,10 +31,24 @@ public class AboutDialog extends Dialog {
     public void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.about);
 
+        PackageInfo p = null;
+        String version = "???";
+        int vCode = 0;
         TextView tv = (TextView) findViewById(R.id.info_text);
         tv.setText(Html.fromHtml(readRawTextFile(R.raw.about_info)));
         tv.setLinkTextColor(Color.BLUE);
         Linkify.addLinks(tv, Linkify.ALL);
+        TextView ver = (TextView) findViewById(R.id.version_string);
+        Context aContext = mContext.getApplicationContext();
+        try {
+            p = aContext.getPackageManager().getPackageInfo(aContext.getPackageName(), 0);
+            version = p.versionName;
+            vCode = p.versionCode;
+        } catch (NameNotFoundException e) {
+            Log.i(TAG, "Failed to retrieve package name");
+            e.printStackTrace();
+        }
+        ver.setText(aContext.getResources().getString(R.string.about_version, version, vCode));
         Button button = (Button) findViewById(R.id.about_ok_button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
