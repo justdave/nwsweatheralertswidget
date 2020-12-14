375527c21db6f089ef33dea4072608393df795f1
Dave Miller <github@justdave.net>
2020-12-14 05:46:08 -0500

    attempt to build only with no emulator test

--- app/src/main/java/net/justdave/nwsweatheralertswidget/AlertsDisplayViewModel.kt
+++ app/src/main/java/net/justdave/nwsweatheralertswidget/AlertsDisplayViewModel.kt
@@ -1,21 +1,9 @@
 package net.justdave.nwsweatheralertswidget
 
-import android.app.Application
 import android.content.Context
-import android.content.res.Resources
-import android.os.AsyncTask
 import android.util.Log
-import android.widget.TextView
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.ViewModel
-import com.android.volley.RequestQueue
 import com.android.volley.Response
-import com.android.volley.toolbox.BasicNetwork
-import com.android.volley.toolbox.DiskBasedCache
-import com.android.volley.toolbox.HurlStack
-import com.android.volley.toolbox.Volley
-import kotlinx.coroutines.Dispatchers
-import org.json.JSONArray
 
 class AlertsDisplayViewModel : ViewModel() {
 
