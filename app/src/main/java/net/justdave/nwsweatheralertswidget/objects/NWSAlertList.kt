95e86dd9dfa67470f8af1c99c90f62e46f937c44
Dave Miller <github@justdave.net>
2020-12-26 15:08:17 -0500

    AlertList class, not using it anywhere yet.

---
+++ app/src/main/java/net/justdave/nwsweatheralertswidget/objects/NWSAlertList.kt
@@ -0,0 +1,74 @@
+package net.justdave.nwsweatheralertswidget.objects
+
+import android.os.Parcel
+import android.os.Parcelable
+import java.util.*
+
+class NWSAlertList() : ArrayList<NWSAlert>(), Parcelable {
+
+    constructor(parcel: Parcel) : this() {
+        arrayListOf<NWSAlert>().apply {
+            parcel.readList(this, NWSAlert::class.java.classLoader)
+        }
+    }
+
+    override fun toString(): String {
+        var result = ""
+        val numAlerts = size
+        result += "There are currently "
+        result += numAlerts.toString()
+        result = "$result active alerts"
+        for (entry in this) {
+            result = """
+                $result
+                ${entry.toString()}
+                """.trimIndent()
+        }
+        return result
+    }
+
+    override fun equals(other: Any?): Boolean {
+        //formatter:off
+        if (this === other) {
+            return true
+        }
+        if (other !is NWSAlertList) {
+            return false
+        }
+        val n = other
+        if (size != n.size) {
+            return false
+        }
+        val N = size
+        for (i in 0 until N) {
+            if (!get(i).equals(n[i])) {
+                return false
+            }
+        }
+        //formatter:on
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return super.hashCode()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeList(this as List<NWSAlert?>)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<NWSAlertList> {
+        override fun createFromParcel(parcel: Parcel): NWSAlertList {
+            return NWSAlertList(parcel)
+        }
+
+        override fun newArray(size: Int): Array<NWSAlertList?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
