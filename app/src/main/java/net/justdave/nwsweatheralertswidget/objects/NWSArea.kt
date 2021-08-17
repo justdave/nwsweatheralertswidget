eb1c6a6c9a647abada53cd0519663c416b389df8
Dave Miller <github@justdave.net>
2021-08-16 22:48:54 -0400

    NWSArea and NWSZone are basic data structures. Treat them as such.

--- app/src/main/java/net/justdave/nwsweatheralertswidget/objects/NWSArea.kt
+++ app/src/main/java/net/justdave/nwsweatheralertswidget/objects/NWSArea.kt
@@ -1,22 +1,10 @@
 package net.justdave.nwsweatheralertswidget.objects
 
-class NWSArea(var id: String, var name: String) {
+data class NWSArea(val id: String, val name: String) {
 
     //to display object as a string in spinner
     override fun toString(): String {
         return name
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (other is NWSArea) {
-            if (other.name === name && other.id === id) return true
-        }
-        return false
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + name.hashCode()
-        return result
-    }
 }
--- app/src/main/java/net/justdave/nwsweatheralertswidget/objects/NWSZone.kt
+++ app/src/main/java/net/justdave/nwsweatheralertswidget/objects/NWSZone.kt
@@ -1,22 +1,9 @@
 package net.justdave.nwsweatheralertswidget.objects
 
-class NWSZone(var id: String, var name: String) {
+data class NWSZone(val id: String, val name: String) {
 
     //to display object as a string in spinner
     override fun toString(): String {
         return name
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (other is NWSZone) {
-            if (other.name === name && other.id === id) return true
-        }
-        return false
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + name.hashCode()
-        return result
     }
 }
