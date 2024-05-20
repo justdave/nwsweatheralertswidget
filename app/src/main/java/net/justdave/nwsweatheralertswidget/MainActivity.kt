dc37bd360011d5612abd0f7da996f95364aefc44
Dave Miller <github@justdave.net>
2024-05-19 22:12:12 -0400

    Let modern SSL work on old versions of Android

--- app/src/main/java/net/justdave/nwsweatheralertswidget/MainActivity.kt
+++ app/src/main/java/net/justdave/nwsweatheralertswidget/MainActivity.kt
@@ -1,10 +1,13 @@
 package net.justdave.nwsweatheralertswidget
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.security.ProviderInstaller
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +16,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        upgradeSecurityProvider()
         setContentView(R.layout.main_activity)
         setSupportActionBar(findViewById(R.id.my_toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         //supportActionBar?.setLogo(R.mipmap.app_icon)
     }
 
+    private fun upgradeSecurityProvider() {
+        ProviderInstaller.installIfNeededAsync(this, object :
+            ProviderInstaller.ProviderInstallListener {
+            override fun onProviderInstalled() {
+            }
+
+            override fun onProviderInstallFailed(errorCode: Int, recoveryIntent: Intent?) {
+                GoogleApiAvailability.getInstance()
+                    .showErrorNotification(this@MainActivity, errorCode)
+            }
+        })
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp()
